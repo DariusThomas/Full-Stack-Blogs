667f64ba96fdd8c23817c690d0b8cfd69c3589dc
Darius Thomas <dariusd.thomas@gmail.com>
2019-05-07 15:59:09 -0400

    gitignore on mailgun

--- src/server/util/email/mailgun.ts
+++ src/server/util/email/mailgun.ts
@@ -4,7 +4,7 @@ let mailgun = mailgunLoader({
     apiKey:"f8a2b5f79ac30097c1740921273129be-e566273b-e4d6b319",
     domain:"sandbox3d3ec1dc973f46b7b46e1efbb2610129.mailgun.org"
 });
-
+//
 export const sendEmail = (to:string, from:string, subject:string, content:string) =>{
     let data ={
         to,
