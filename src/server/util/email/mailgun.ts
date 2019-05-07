4df26518125791a4a68bd30ba43702216cbc8d4a
Darius Thomas <dariusd.thomas@gmail.com>
2019-05-07 16:00:04 -0400

    exp cred

--- src/server/util/email/mailgun.ts
+++ src/server/util/email/mailgun.ts
@@ -1,8 +1,8 @@
 import * as mailgunLoader from "mailgun-js"
 
 let mailgun = mailgunLoader({
-    apiKey:"f8a2b5f79ac30097c1740921273129be-e566273b-e4d6b319",
-    domain:"sandbox3d3ec1dc973f46b7b46e1efbb2610129.mailgun.org"
+    apiKey:"",
+    domain:""
 });
 //
 export const sendEmail = (to:string, from:string, subject:string, content:string) =>{
