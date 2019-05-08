1bdec772a70a5518668af1e6c7a24796e323438f
Darius Thomas <dariusd.thomas@gmail.com>
2019-05-08 12:29:35 -0400

    brute force true pw for now to test other functionality

--- src/server/middleware/localstrategy.ts
+++ src/server/middleware/localstrategy.ts
@@ -9,8 +9,8 @@ let ls = new LocalStrategy.Strategy({
 }, async (email, password, done) => {
     try {
         let [user]: any = await DB.Authors.userByEmail(email);
-        console.log(user,password,ComparePassword(password, user.password) )
-        if (user && ComparePassword(password, user.password)) {
+        //console.log(user,password,ComparePassword(password, user.password) )
+        if (user && true /*ComparePassword(password, user.password)*/) {
             let token = await CreateToken({ userid: user.id })
             let response = {
                 token,
