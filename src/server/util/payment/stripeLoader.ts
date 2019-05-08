90cc6413255bdcbec038d9b927a6fdac8bc613b0
Darius Thomas <dariusd.thomas@gmail.com>
2019-05-08 14:26:18 -0400

    resolving donation error prompt

--- src/server/util/payment/stripeLoader.ts
+++ src/server/util/payment/stripeLoader.ts
@@ -1,6 +1,6 @@
 import * as stripeLoader from "stripe"
 
-const stripe = new stripeLoader("sk_test_aFlAqZoWcG5BuiU7AYbqTvDk00w2QpYopp"/*process.env.STRIPE_SK*/);
+const stripe = new stripeLoader(process.env.STRIPE_SK);
 
 export const charge = (token:string, amt:number) =>{
     return stripe.charges.create({
